ab58d67c03854b410fe4ca5c06b243a6d91b41d0
ArctikkSunrise <kanokwankullawan@gmail.com>
2015-05-09 17:44:02 +0200

    Assignment 2 - May2015

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    #set the value of the vector
     m <- NULL
     set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    #get the value of the vector
     get <- function() x
+    #set and get the value of the mean
     setinv <- function(solve) m <<- solve
     getinv <- function() m
     list(set = set, get = get,
@@ -24,10 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     m <- x$getinv()
+    # if the mean has already been calculated, gets the mean from the cache and skips the computation
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    # if mean hasn't already calculated, calculate mean
     data <- x$get()
     m    <- solve(data, ...)
     x$setinv(m)
